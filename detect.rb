9c896a39401b063dc0a8dd52b29d413af5218c59
Daniel Schömer <daniel.schoemer@gmx.net>
2013-09-22 23:06:04 +0200

    Comment about used gems.

--- detect.rb
+++ detect.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 # vim:fileencoding=UTF-8 shiftwidth=2:
 
+# This script needs rubygems:
+#  - open4  >= 1.3.0
+#  - thread >= 0.1.1
+
 require 'ipaddr'
 require 'open4'
 require 'pp'
