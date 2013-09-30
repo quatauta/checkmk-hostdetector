a10b8daeb7e440c52b526f8f46532dfe2ae924d4
Daniel Schömer <daniel.schoemer@gmx.net>
2013-09-30 15:46:39 +0200

    Added cli help

--- lib/checkmk/devicedetector/cli.rb
+++ lib/checkmk/devicedetector/cli.rb
@@ -31,23 +31,41 @@ module CheckMK
       end
 
       def parse_options_slop(argv = ARGV)
-        options = Slop.new :help => true, :multiple_switches => true
+        options = Slop.new help: true, multiple_switches: true
 
-        options.banner \
-          "Scans your network for devices and builds suitable configuration for CheckMK/WATO." + "\n\n" +
-          "Usage:" + "\n" +
-          "  #{$PROGRAM_NAME} [-c CONFIG-FILE] [-j JOBS] [-l LOCATIONS-FILE]" + "\n" +
-          "  #{$PROGRAM_NAME} [-c CONFIG-FILE] [-j JOBS] <LOCATIONS-FILE"     + "\n" +
-          "  echo 'Local 192.168.0.0/24' | #{$PROGRAM_NAME} [-c CONFIG-FILE] [-j JOBS]" + "\n"
+        options.banner <<-END
+          Scans your network for devices and builds suitable configuration for
+          CheckMK/WATO.
 
-        options.on('c=', 'config=', 'The configuration file to use',
-                   :default => 'config.rb')
+          Usage:
+            #{$PROGRAM_NAME} [OPTIONS] [-l] RANGE-FILES
+
+          STDIN is read if location file is '-' or omitted:
+            #{$PROGRAM_NAME} [OPTIONS] < RANGE-FILE
+            cat RANGE-FILES | #{$PROGRAM_NAME} [OPTIONS] [-r -]
+            echo 'Local 192.168.0.0/24' | #{$PROGRAM_NAME} [OPTIONS] [-r -]
+
+          Range files contain a name and the IP adress ranges to be scanned. Each line
+          begins with the name followed by one or multiple IP address ranges. Name and
+          range(s) are divided by whitespace. The ranges must conform to the nmap [1]
+          target specifications.
+
+          [1] http://nmap.org/book/man-target-specification.html
+
+          The options listed below may be specified indifferent ways like shown in this
+          examples:  -ca.rb  -c a.rb  -c=a.rb  --c a.rb  --c=a.rb
+                     -config a.rb  -config=a.rb  --config a.rb  --config=a.rb
+          END
+          .gsub(/^          /, '')
+        options.on('c=', 'config=', 'The configuration file(s) to use',
+                   as: Array, default: ['config.rb'])
         options.on('j=', 'jobs=', 'The maximum number of jobs run in parallel',
-                   :as => Integer, :default => 4)
-        options.on('l=', 'locations=', 'The file(s) containing a list of locations to be scanned',
-                   :as => Array, :default => ['locations.txt'])
+                   as: Integer, default: 4)
+        options.on('r=', 'ranges=', 'The file(s) containing ranges to be scanned',
+                   as: Array, default: ['ranges.txt'])
 
         options.parse(argv)
+        require 'pp' ; pp options.to_hash
         options
       end
     end
