48a70db8ad65498a209b8177249166fb37d7491a
Daniel Schömer <daniel.schoemer@gmx.net>
2013-09-30 16:49:07 +0200

    Updated autoloads.

--- lib/checkmk/devicedetector.rb
+++ lib/checkmk/devicedetector.rb
@@ -3,10 +3,13 @@
 
 module CheckMK
   module DeviceDetector
-    autoload :Config,   'checkmk/devicedetector/config'
-    autoload :Detector, 'checkmk/devicedetector/detector'
-    autoload :Device,   'checkmk/devicedetector/device'
-    autoload :Helper,   'checkmk/devicedetector/helper'
-    autoload :Site,     'checkmk/devicedetector/site'
+    autoload :Cli,           'checkmk/devicedetector/cli'
+    autoload :Config,        'checkmk/devicedetector/config'
+    autoload :Configuration, 'checkmk/devicedetector/configuration'
+    autoload :Detector,      'checkmk/devicedetector/detector'
+    autoload :Device,        'checkmk/devicedetector/device'
+    autoload :Helper,        'checkmk/devicedetector/helper'
+    autoload :Site,          'checkmk/devicedetector/site'
+    autoload :VERSION,       'checkmk/devicedetector/version'
   end
 end
