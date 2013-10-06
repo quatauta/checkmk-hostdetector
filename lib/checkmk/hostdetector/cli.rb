6013f3f3bb5ba73256c53e5abc8f4d4a21724be7
Daniel Schömer <daniel.schoemer@gmx.net>
2013-10-06 10:17:56 +0200

    Corrected and clarified cli help output.

--- lib/checkmk/hostdetector/cli.rb
+++ lib/checkmk/hostdetector/cli.rb
@@ -38,19 +38,21 @@ module CheckMK
           CheckMK/WATO.
 
           Usage:
-            #{$PROGRAM_NAME} [OPTIONS] [-l] SITE-FILES
+            #{$PROGRAM_NAME} [OPTIONS] [-s SITES-FILE...] [SITE-FILES...]
 
           STDIN is read if site file is '-' or omitted:
-            #{$PROGRAM_NAME} [OPTIONS] < SITES-FILE
-            cat SITES-FILES | #{$PROGRAM_NAME} [OPTIONS] [-r -]
-            echo 'Local 192.168.0.0/24' | #{$PROGRAM_NAME} [OPTIONS] [-r -]
+            #{$PROGRAM_NAME} [OPTIONS] [-s -] [-] < SITES-FILE
+            cat SITES-FILES... | #{$PROGRAM_NAME} [OPTIONS] [-]
+            echo 'Local 192.168.0.0/24' | #{$PROGRAM_NAME} [OPTIONS] [-]
 
           Site files contain a name and the IP adress ranges to be scanned. Each line
           begins with the site's name followed by one or multiple IP address ranges. Name
           and range(s) are divided by whitespace. The ranges must conform to the nmap [1]
-          target specifications.
+          target specifications. Multiple lines the contain the same site's name are
+          merged.
 
-            Site-A 10.0.1.0/24 10.0.100.0/24 192.168.0,1,2.1-254
+            Site-A 10.0.1.0/24 10.0.100.0/24
+            Site-A 192.168.0,1,2.1-254
             Site-B 10.0.2.0/24 10.0.200.0/24
             Site-C 10.0.3.0/24 10.0.300.0/24
 
@@ -59,13 +61,16 @@ module CheckMK
           The options listed below may be specified indifferent ways like shown in this
           examples:  -ca.rb  -c a.rb  -c=a.rb  --c a.rb  --c=a.rb
                      -config a.rb  -config=a.rb  --config a.rb  --config=a.rb
+
+          You can specify more than one configuration file. Each file is read in the given
+          order and settings are overridden/merged in order of read files.
           END
           .gsub(/^          /, '')
-        options.on('c=', 'config=', 'The configuration file(s) to use',
+        options.on('c=', 'config=', 'The configuration file(s) to use.',
                    as: Array, default: ['config.rb'])
         options.on('j=', 'jobs=', 'The maximum number of jobs run in parallel',
                    as: Integer, default: 4)
-        options.on('s=', 'sites=', 'The file(s) containing sites to be scanned',
+        options.on('s=', 'sites=', 'The file containing sites to be scanned',
                    as: Array, default: ['sites.txt'])
 
         options.parse(argv)
