6594dd55bd67746b9ac7366fade3922257e48620
Daniel Schömer <daniel.schoemer@gmx.net>
2013-10-16 21:21:32 +0200

    Updated spec for ConfigDSL.

--- spec/checkmk/hostdetector/config_dsl_spec.rb
+++ spec/checkmk/hostdetector/config_dsl_spec.rb
@@ -2,12 +2,46 @@ require 'checkmk/hostdetector/config_dsl'
 
 module CheckMK::HostDetector
   describe ConfigDSL do
+    describe "#initialize" do
+      it "is empty after creation" do
+        expect(subject.instance_variables).to be_empty
+        expect(subject.methods).to be_nil
+      end
+
+      it "returns nil on undefined options" do
+        expect(subject.option_a).to be_nil
+        expect(subject.option_b).to be_nil
+        expect(subject.option_c).to be_nil
+      end
+    end
+
     describe "#load" do
-      it "loads configuration from string" do
-        config = ConfigDSL.new
-        config.test_config.should == nil
-        config.load 'test_config "123"'
-        config.test_config.should == ["123"]
+      it "loads one option into array" do
+        subject.load('option_a "123"')
+        expect(subject.option_a).to match_array(["123"])
+      end
+
+      it "loads multiple options into array" do
+        subject.load('option_a "123"')
+        subject.load('option_a "234"')
+        subject.load('option_a "345"')
+        expect(subject.option_a).to match_array(["123", "234", "345"])
+      end
+
+      it "loads multiple options of different types into array" do
+        subject.load('option_a "123"')
+        subject.load('option_a 234')
+        subject.load('option_a /345/')
+        expect(subject.option_a).to match_array(["123", 234, /345/])
+      end
+    end
+
+    describe "#load_from_file" do
+      it "reads file content " do
+        filename = File.join(File.dirname(__FILE__), '..', '..', 'fixtures', 'config_dsl_example.rb')
+        subject.load_file(filename)
+        expect(subject.option_a).to match_array(["Is a string", "or", "some", "more strings"])
+        expect(subject.option_b).to match_array([123, 234, /Peter/])
       end
     end
   end
---
+++ spec/fixtures/config_dsl_example.rb
@@ -0,0 +1,7 @@
+option_a "Is a string"
+option_a "or"
+option_a "some"
+option_a "more strings"
+option_b 123
+option_b 234
+option_b /Peter/
