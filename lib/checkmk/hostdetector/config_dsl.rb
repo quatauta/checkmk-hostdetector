b18d65867a554390119659d9496e7197d774b9db
Daniel Schömer <daniel.schoemer@gmx.net>
2016-10-16 11:05:59 +0200

    Fixed some rubocop remarks

--- lib/checkmk/hostdetector/config_dsl.rb
+++ lib/checkmk/hostdetector/config_dsl.rb
@@ -1,20 +1,20 @@
 # -*- coding: utf-8; -*-
+# frozen_string_literal: true
 # vim:set fileencoding=utf-8:
 
 module CheckMK
   module HostDetector
     class ConfigDSL
-
-      KEEP_METHODS = %w[ __id__ __send__ class inspect instance_eval instance_variables object_id send == === eql to_s ].map { |str| str.to_sym }
+      KEEP_METHODS = %w( __id__ __send__ class inspect instance_eval
+                         instance_variables object_id send
+                         == === eql to_s ).map(&:to_sym)
 
       instance_methods.each do |method|
-        unless KEEP_METHODS.include? method
-          undef_method(method)
-        end
+        undef_method(method) unless KEEP_METHODS.include? method
       end
 
       def self.dsl_accessor(*symbols)
-        symbols.each { |sym|
+        symbols.each do |sym|
           class_eval %{
             def #{sym}(*val)
               if val.empty?
@@ -30,12 +30,16 @@ module CheckMK
               end
             end
           }
-        }
+        end
       end
 
       def method_missing(sym, *args)
         self.class.dsl_accessor sym
         send(sym, *args)
+      end
+
+      def respond_to_missing?(method_name, include_private = false)
+        self.methods.inclde(method_name) || super
       end
 
       def load(text, filename = '(inline string)')
